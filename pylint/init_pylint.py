432d9d67ea2de9dd1ce9df532d03ebe2e3f66b51
Michael K. Johnson <http://issues.rpath.com/>
2008-04-29 15:02:56 -0400

    remove unnecessary path insertion for pylint

--- pylint/init_pylint.py
+++ pylint/init_pylint.py
@@ -1,7 +1,6 @@
 import os
 import sys
 sys.path.insert(0, os.environ['CONARY_PATH'])
-sys.path.insert(0, os.environ['RMAKE_PATH'])
 sys.path.insert(0, os.environ['PRODUCT_DEFINITION_PATH'])
 sys.path.insert(0, os.environ['XMLDATA_PATH'])
 
