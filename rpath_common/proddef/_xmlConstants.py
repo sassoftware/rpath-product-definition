b912c15fc106733b64a9eb3d7882f9b8d825e4ee
Mihai Ibanescu <https://issues.rpath.com/>
2008-05-01 14:09:56 -0400

    Additional files for handling image types

---
+++ rpath_common/proddef/_xmlConstants.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2006-2008 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+"""
+@var defaultNamespace: Default namespace
+@var xmlSchemaNamespace: XML Schema namespace
+@var xmlSchemaLocation: XML Schema location
+"""
+
+defaultNamespace = "http://www.rpath.com/permanent/rpd.xsd"
+xmlSchemaNamespace = "http://www.w3.org/2001/XMLSchema-instance"
+xmlSchemaLocation = "http://www.rpath.com/permanent/pd.xsd pd.xsd"
