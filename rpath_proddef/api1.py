7ff03eb8e4913778a9a47e716bbece7ee9cea994
Adam Chasen <achasen@rpath.com>
2011-07-13 10:47:53 -0400

    RPCL-78 default publishUpstreamPlatformSearchPaths default to False

--- rpath_proddef/api1.py
+++ rpath_proddef/api1.py
@@ -1901,7 +1901,7 @@ class ProductDefinitionRecipe(PackageRecipe):
 
     def getPublishUpstreamPlatformSearchPaths(self):
         if self._rootObj.publishUpstreamPlatformSearchPaths is None:
-            return True
+            return False
         return bool(self._rootObj.publishUpstreamPlatformSearchPaths)
 
     def setPublishUpstreamPlatformSearchPaths(self, value):
